7c1ce616ddf802721757f9365b777e9e3c4c6f71
Tasbeeh <tasbeehsaeed2018@gmail.com>
2025-04-21 08:23:52 +0300

    Create q5.java

---
+++ q5.java
@@ -0,0 +1,7 @@
+public static void simpleForLoop (int upperLimit){ //method
+final int START = 1;
+    for(int i = START; i <= upperLimit; i++){
+        System.out.println(i);  // print i
+    }
+} //final    
+    
