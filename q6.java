4e5d74de251cf202822b1342fe008fbe796cdc33
Tasbeeh <tasbeehsaeed2018@gmail.com>
2025-04-21 08:24:44 +0300

    Create q6.java

---
+++ q6.java
@@ -0,0 +1,11 @@
+public static void simpleWhileLoop(int upperLimit){ //name of method
+        // TODO code application logic here
+        int START =1;
+        
+       while(START <= upperLimit){//print the number
+           System.out.println(START);
+           START++;
+       }//final
+}
+    
+
